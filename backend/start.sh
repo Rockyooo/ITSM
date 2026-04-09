0582ce177ac645959c8dd070237cdbd5bc20a408
Rockyooo <oscar.rojas1620@gmail.com>
2026-04-09 17:38:12 -0500

    fix: start.sh sin BOM

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,3 @@
-﻿#!/bin/sh
-echo "Creando tablas..."
-python -c "
-from app.database import engine
-from app.models import Base
-Base.metadata.create_all(bind=engine)
-print('Tablas creadas OK')
-"
-echo "Iniciando servidor..."
+#!/bin/sh
+echo 'Iniciando servidor...'
 uvicorn app.main:app --host 0.0.0.0 --port 8000
